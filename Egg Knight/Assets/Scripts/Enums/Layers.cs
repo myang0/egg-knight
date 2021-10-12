df3f8ebf116c886f3f8774248ba695801dcb5c8e
mwyang <matthew_yang@sfu.ca>
2021-10-11 22:57:30 -0700

    add cooldown for roll, roll now disables collision with hostile enemies

---
+++ Egg Knight/Assets/Scripts/Enums/Layers.cs
@@ -0,0 +1,13 @@
+public enum Layers {
+  Default,
+  TransparentFX,
+  IgnoreRaycast,
+  Player,
+  Water,
+  UI,
+  PlayerWeapon,
+  PlayerProjectile,
+  Enemy,
+  EnemyWeapon,
+  EnemyProjectile,
+}
