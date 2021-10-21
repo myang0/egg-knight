2b5ce9a4e9969aec9db9d1b78d4b52e3a7dcb2b0
mwyang <matthew_yang@sfu.ca>
2021-10-21 15:58:57 -0700

    player weapons are no longer collision based

--- Egg Knight/Assets/Scripts/Player/Combat/Weapons/Fork/TwoProngedFork.cs
+++ Egg Knight/Assets/Scripts/Player/Combat/Weapons/Fork/TwoProngedFork.cs
@@ -1,5 +1,27 @@
 using UnityEngine;
 
 public class TwoProngedFork : BasePlayerWeapon {
+  [SerializeField] private Transform _attackPoint;
+  [SerializeField] private float _attackWidth;
+  [SerializeField] private float _attackHeight;
 
+  [SerializeField] private LayerMask _enemyLayer;
+
+  public override void EnableHitbox() {
+    float hitboxAngle = transform.eulerAngles.z;
+
+    Collider2D[] enemiesInRange = Physics2D.OverlapBoxAll(
+      _attackPoint.position,
+      new Vector2(_attackWidth, _attackHeight),
+      hitboxAngle,
+      _enemyLayer
+    );
+
+    DamageEnemies(enemiesInRange);
+  }
+
+  protected override void OnDrawGizmosSelected() {
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireCube(_attackPoint.position, new Vector3(_attackWidth, _attackHeight, 1));
+  }
 }
