654f34d40a36baa33326f7df6abe61e341f452f2
mwyang <matthew_yang@sfu.ca>
2021-10-23 02:24:27 -0700

    add particle effects for on hit effects

---
+++ Egg Knight/Assets/Scripts/Enemies/EnemyStatusParticles.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class EnemyStatusParticles : MonoBehaviour {
+  [SerializeField] private GameObject _yolkParticles;
+  [SerializeField] private GameObject _igniteParticles;
+  [SerializeField] private GameObject _frostParticles;
+  [SerializeField] private GameObject _electrocuteParticles;
+
+  private void Awake() {
+    EnemyBehaviour enemyBehaviour = gameObject.GetComponent<EnemyBehaviour>();
+
+    enemyBehaviour.OnYolked += HandleYolked;
+    enemyBehaviour.OnIgnited += HandleIgnited;
+    enemyBehaviour.OnFrosted += HandleFrosted;
+    enemyBehaviour.OnElectrocuted += HandleElectrocuted;
+  }
+
+  private void HandleYolked(object sender, EventArgs e) {
+    if (_yolkParticles != null) {
+      Instantiate(_yolkParticles, transform.position, Quaternion.identity);
+    }
+  }
+
+  private void HandleIgnited(object sender, EventArgs e) {
+    if (_igniteParticles != null) {
+      Instantiate(_igniteParticles, transform.position, Quaternion.identity);
+    }
+  }
+
+  private void HandleFrosted(object sender, EventArgs e) {
+    if (_frostParticles != null) {
+      Instantiate(_frostParticles, transform.position, Quaternion.identity);
+    }
+  } 
+
+  private void HandleElectrocuted(object sender, EventArgs e) {
+    if (_electrocuteParticles != null) {
+      Instantiate(_electrocuteParticles, transform.position, Quaternion.identity);
+    }
+  }
+}
