b7bdd5d50c2b59aea13e46b93c27f418faf666ec
mwyang <matthew_yang@sfu.ca>
2021-11-23 22:20:29 -0800

    bishop bacon

--- Egg Knight/Assets/Scripts/Enemies/Bacon/BaconAttack.cs
+++ Egg Knight/Assets/Scripts/Enemies/Bacon/BaconAttack.cs
@@ -1,11 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class BaconAttack : MonoBehaviour {
+  [SerializeField] private float _cooldownTime;
+
   [SerializeField] private GameObject _greaseOrbPrefab;
+
+  private Animator _anim;
+
+  private void Awake() {
+    _anim = GetComponent<Animator>();
+  }
 
   public void SpawnOrb() {
     if (_greaseOrbPrefab != null) {
       Instantiate(_greaseOrbPrefab, transform.position, Quaternion.identity);
+
+      StartCoroutine(Cooldown());
     }
+  }
+
+  private IEnumerator Cooldown() {
+    yield return new WaitForSeconds(_cooldownTime);
+
+    _anim.SetBool("IsAttacking", false);
   }
 }
