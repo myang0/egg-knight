fdd3867bd299f42fdc2b64dd480e58eb18218cad
mwyang <matthew_yang@sfu.ca>
2021-11-28 23:11:34 -0800

    rework sherrif sausage bomb attack

--- Egg Knight/Assets/Scripts/Enemies/Sausage/Attacks/SmokeBomb.cs
+++ Egg Knight/Assets/Scripts/Enemies/Sausage/Attacks/SmokeBomb.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class SmokeBomb : MonoBehaviour {
   private Rigidbody2D _rb;
+
+  [SerializeField] private GameObject _crosshairObject;
+  private Crosshair _currentCrosshair;
 
   [SerializeField] private GameObject _smokeParticlesObject;
 
@@ -19,13 +23,14 @@ public class SmokeBomb : MonoBehaviour {
     _rb = GetComponent<Rigidbody2D>();
 
     Vector2 forceVector = Quaternion.Euler(0, 0, Random.Range(-10f, 10f)) * Vector2.up;
-    _rb.AddForce(forceVector * Random.Range(15f, 20f), ForceMode2D.Impulse);
-
+    _rb.AddForce(forceVector * Random.Range(50f, 75f), ForceMode2D.Impulse);
     _rotationSpeed = Random.Range(-10f, 10f);
 
-    _targetY = transform.position.y + Random.Range(-3f, 3f);
+    _targetY = transform.position.y;
 
     SausageHealth.OnSausageDeath += HandleBossDeath;
+
+    StartCoroutine(DropTimer());
   }
 
   private void HandleBossDeath(object sender, EventArgs e) {
@@ -37,6 +42,10 @@ public class SmokeBomb : MonoBehaviour {
 
     if (transform.position.y <= _targetY && _rb.velocity.y < 0) {
       Explode();
+
+      if (_currentCrosshair != null) {
+        _currentCrosshair.FadeOut();
+      }
     }
   }
 
@@ -60,5 +69,25 @@ public class SmokeBomb : MonoBehaviour {
 
   private void OnDestroy() {
     SausageHealth.OnSausageDeath -= HandleBossDeath;
+  }
+
+  private IEnumerator DropTimer() {
+    float waitTime = Random.Range(0.5f, 1f);
+    yield return new WaitForSeconds(waitTime);
+
+    Drop();
+  }
+
+  private void Drop() {
+    _rb.velocity = Vector2.zero;
+
+    Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+    
+    Vector3 target = new Vector3(playerPos.x + Random.Range(-4f, 4f), playerPos.y + Random.Range(-4f, 4f), playerPos.z);
+    transform.position = new Vector3(target.x, target.y + 50, target.z);
+
+    _targetY = target.y;
+
+    _currentCrosshair = Instantiate(_crosshairObject, target, Quaternion.identity).GetComponent<Crosshair>();
   }
 }
